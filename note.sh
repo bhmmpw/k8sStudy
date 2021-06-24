9bfb3660a1b52a4e90a033660906d5c82b848253
k3s <k3s@example.com>
2021-06-24 16:47:18 +0900

    update note

--- note.sh
+++ note.sh
@@ -81,6 +81,8 @@ ReadWriteOnece가 Read 는 무제한이나 쓰는 것은 한번쓰면 그다음�
 쎄마포어같은건가보네요
 BS_장준표 님이 모두에게:    오전 9:37
 pv를 생성하면 실제 volume이 생성되는 위치는 어디인지 알수 있나요?( 어느 노드에 생성되는지?)
+구세완 님이 모두에게:    오전 10:38
+교재는 컨테이너1, 컨테이너2 로 되어있는데요. 교재를 수정해야 하는지요? 
 구세완 님이 모두에게:    오전 10:40
 세번째 문장에도 그런데요. 
 구세완 님이 모두에게:    오전 10:40
@@ -91,3 +93,131 @@ pv를 생성하면 실제 volume이 생성되는 위치는 어디인지 알수 �
 serviceip -> kube-proxy의 동작은 이해하고 있습니다. 
 신동렬/LG유플러스 님이 모두에게:    오전 10:45
 아 넵 감사합니다. 
+신동렬/LG유플러스 님이 모두에게:    오전 11:29
+cbr과 cni는 어떻게 다른건가요? 
+CTO_김성찬 님이 모두에게:    오전 11:31
+cbr 이라면 custom bridge 를 의미하나요
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오전 11:32
+calico 썼습니다.
+신동렬/LG유플러스 님이 모두에게:    오전 11:32
+calico, weavenet 써봣습니다 
+구세완 님이 모두에게:    오전 11:33
+weave, multus, Flannel 인스톨해봤는데요. 
+구세완 님이 모두에게:    오전 11:34
+멀티캐스팅이 내부적으로 되는 것을 찾았는데, multus가 된다고 들어서..
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오전 11:35
+몇개의 노드에 몇천개의 pod 구성해서 실시간 부하를 거는 용으로 사용해도 특별하게 문제가 없어서 다른걸 안써봤습니다.
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오전 11:50
+on-premise에서 진행했습니다.
+강사 김대경 님이 모두에게:    오전 11:50
+고생하셨습니다. 점심 식사 맛있게 하세요
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 1:08
+마스터에서는 보이나요?
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 1:09
+옵션때문인건지 실제 pod이 배포된곳에서만 보이는건지
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 1:09
+구분이 안가서..
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:10
+버전은 kubernetes 버전을 말씀하신건가요? 
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 1:10
+저는 지금 master에 배포되었고 master에서만 보이는 상태거든요
+BS_장준표 님이 모두에게:    오후 1:18
+강사님 network pocliy yaml 파일에서 ingress 부분 좀 설명 부탁 드립니다.
+강사 김대경 님이 모두에게:    오후 1:23
+kubectl apply -f https://docs.projectcalico.org/manifests/canal.yaml
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:29
+podSelector 가 없으면 아무곳(어떤 pod)에도 적용하지 않는다는 의미가 아닌가요?
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:46
+network policy 가 적용(실행)되는 위치가 어디인가요? kube-proxy? kubelet?
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:51
+방금 pod 안에서 호출한 방식도 master(kubelet)을 경유한 건가요? 보이는건 바로 pod ip 로 호출한것처럼 보여서요?
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:51
+네
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:53
+네
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:53
+실습은 client -> web, client->non-web ip로 바로 요청한것으로 보여서요
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:54
+저렇게 되면 network policy가 개입할 위치가 없지 않나해서요
+구세완 님이 모두에게:    오후 1:54
+수업하는 networkpolicy  클러스터 내부에 대한 것인지, 혹은 외부에서 들어오는 것도 제어가 가능한지요?
+강사 김대경 님이 모두에게:    오후 1:55
+client -> web 에서 -> 부분에서 제어 역할이 있게 됩니다.
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:55
+저도 cluster 외부에서 들어오는 ingress에 관련된 내용으로 보여서요.
+구세완 님이 모두에게:    오후 1:55
+예
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:55
+내부 pod to pod는 아닌것 같은 느낌이...
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 1:59
+네 말씀감사합니다.
+윤태희/책임/빅데이터엔지니어링TP 님이 모두에게:    오후 2:10
+Networkpolicy가 적용되는 범위는 cluster 레벨인가요 namespace 레벨인가요?
+윤태희/책임/빅데이터엔지니어링TP 님이 모두에게:    오후 2:14
+Networkpolicy 정의 YAML에 namespace 를 정의하는 부분이 있어서 여쭤봅니다
+신동렬/LG유플러스 님이 모두에게:    오후 2:15
+namespace 기준으로 알고 있고, network policy에서 namespace영역을 지정하지 않으면, default namespace로 자동 정의되는 것으로 이해하고 있습니다. 
+윤태희/책임/빅데이터엔지니어링TP 님이 모두에게:    오후 2:15
+네 감사합니다
+신동렬/LG유플러스 님이 모두에게:    오후 2:15
+kubectl get all 을 했을때도 namespace를 지정하지 않고 호출하면 default의 namespace에 상세만 출력됩니다. 
+김민재 책임/webOS platform TasK 님이 모두에게:    오후 2:18
+pod에 특정 라벨이 추가되어있는지 확인 할수있는 방법은 없나요?
+신동렬/LG유플러스 님이 모두에게:    오후 2:25
+kubectl get pod [pod명] -n [namespace] --show-labels 로 확인해보시면 되지 않을까 합니다. 
+김민재 책임/webOS platform TasK 님이 모두에게:    오후 2:26
+감사합니다.
+CTO_김성찬 님이 모두에게:    오후 2:34
+이 예제는 아까 설치한 canal 과는 상관 없이 동작하는건가요
+강사 김대경 님이 모두에게:    오후 2:47
+kubectl expose deployment hello-world --type=NodePort --name=example-service
+강사 김대경 님이 모두에게:    오후 2:49
+kubectl get pods --selector="run=load-balancer-example" --output=wide
+구세완 님이 모두에게:    오후 2:51
+hello-world 는 그전 api-gateway 와는 무관한건가요? 
+구세완 님이 모두에게:    오후 2:51
+어떤 연결고리가 있는지 안보여서요.
+신동렬/LG유플러스 님이 모두에게:    오후 2:54
+아까 yaml 보면 그냥 서비스 로드밸런서를 명칭을 그냥 api-gateway라고 명명하신것만 같아보였스빈다 
+BS_장준표 님이 모두에게:    오후 2:57
+api-gateway.yaml 파일은 서비스 오브젝트 생성 코드 같은데 포드 까지 같이 배포되는건 무엇 때문인가요?
+강사 김대경 님이 모두에게:    오후 2:58
+hello-application 말씀하시는 거죠?
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 3:02
+강사님 말씀하시는거 아니시죠? 마이크가 꺼져있는데요.
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 3:02
+혹시 저만 오디오
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 3:02
+안나오는거 아니죠?
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 3:02
+강사님이 mute 깜빡하신것 같습니다
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 3:02
+처음부터 다시 하셔야겠네요.
+박상욱책임/생체인지Task/CTO 님이 모두에게:    오후 3:23
+metrics 서비스도 설치하나요?
+구세완 님이 모두에게:    오후 3:28
+지금까지 설치한 yaml 전체가 몇개인가요?
+CTO부문_백지웅 님이 모두에게:    오후 3:37
+그라파나 id pw 어떤걸로 넣어야 하나요
+강사 김대경 님이 모두에게:    오후 3:38
+admin/admin
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 3:48
+여기에는 왜 외부주소가 아니라 내부주소를 입력해줘야 하는건가요?
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 3:49
+pod 내부 통신이라는거죠?
+CTO부문_백지웅 님이 모두에게:    오후 3:52
+import 부분 어떤걸 import 해야하나요
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 3:52
+여기서 보이는 resource들은 특정 node 인거죠? pod기준이 아니라
+김민재 책임/webOS platform TasK 님이 모두에게:    오후 3:53
+프로메테우스의 내부 아이피를 연결하는 부분 다시 설명 부탁 드립니다. 
+신동렬/LG유플러스 님이 모두에게:    오후 3:58
+방금 프로메테우스를 nodeport로 지정하고 clusterip를 
+김민재 책임/webOS platform TasK 님이 모두에게:    오후 3:58
+네 감사합니다.
+신동렬/LG유플러스 님이 모두에게:    오후 3:58
+지정하였는데 
+신동렬/LG유플러스 님이 모두에게:    오후 3:58
+nodeport를 지정안하고 그냥 배포한 상태에서 clusterip로 등록하고자 한다면 가능한다가요? nodeport를 지정안하면 안되겟죠? 
+신동렬/LG유플러스 님이 모두에게:    오후 3:59
+넵 이해했습니다
