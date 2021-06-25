3f96c49d456ab511808b36e8741c57649726d995
k3s <k3s@example.com>
2021-06-25 16:33:06 +0900

    update day5 note

--- day5_note.sh
+++ day5_note.sh
@@ -26,12 +26,12 @@ CTO_김성찬 님이 모두에게:    오전 9:05
 export POD_NAME=$(kubectl get pods --namespace dev -l "app.kubernetes.io/name=hello-world,app.kubernetes.io/instance=hello-world" -o jsonpath="{.items[0].metadata.name}")
 강사 김대경 님이 모두에게:    오전 9:06
 export CONTAINER_PORT=$(kubectl get pod --namespace dev $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
-강사 김대경 님이 모두에게:    오전 9:06
-kubectl --namespace dev port-forward $POD_NAME 8080:$CONTAINER_PORT
-CTO_김성찬 님이 모두에게:    오전 9:08
-민경직 선임님, 네 그러네요  감사합니다
-민경직/선임/webOS Platform Task 님이 모두에게:    오전 9:19
-현업 적용이 어려운거 아닌가요 그러면
+
+
+
+
+
+
 구세완 님이 모두에게:    오전 9:33
 chart version, app version 차이가 있는데, 각각 어떤 의미인지요? helm install 시에 둘 다 저장 혹은 구분을 하나요?
 구세완 님이 모두에게:    오전 9:34
@@ -147,4 +147,60 @@ webex 445 님이 모두에게:    오전 11:51
 [설문안내] 금요일 5시 교육 종료시 시스템 접속이 원활하지 않은 경우가 많아, 미리 안내 드리오니, 쉬는시간을 활용하여 전원 참여를 부탁 드립니다. :) 
 ☞ 설문링크:   http://survey.lge.com/limesurvey/index.php?r=survey/index&sid=388277&lang=ko 
 or http://survey.lge.com/접속 후, Class code : P00000495
-메일로도 안내 드렸습니다!
+메일로도 안내 드렸습니다! 
+구세완 님이 모두에게:    오후 1:08
+현재 운영중인 deployment.yaml을 helm 으로 바꾸려면 어떻게 접근을 하여 어디를 수정해야할지 잘 모르겠습니다. 
+구세완 님이 모두에게:    오후 1:10
+간단한 어제했던 echo 라도 보여주시면 도움이 될듯 합니다. 
+구세완 님이 모두에게:    오후 1:11
+아 제 의미는 제가 갖고 있는 pod 나 deployment  를 어떻게 helm 에 맞도록 수정해야 할지입니다.
+강사 김대경 님이 모두에게:    오후 1:11
+네 준비되면 말씀드리도록 하겠습니다.
+강사 김대경 님이 모두에게:    오후 1:11
+아 ! 네..
+구세완 님이 모두에게:    오후 1:23
+values.yaml 에 하나의 이미지만 설정이 가능한가요?  아니면 --- 등을 통해서 여러 이미지에 대한 것도 넣을 수 있는지요?  혹은 values2.yaml, values3.yaml 을 만들수 있나요?   요약해서 질문드리면  3개 앱이 있으면 3개 차트를 create  해야할지 하나를 만들어서 사용할 수 있는지에 대한 것입니다.
+강사 김대경 님이 모두에게:    오후 1:26
+네..질문하신 내용들에 대해 종합해서 정리해 말씀드리도록 하겠습니다.
+BS_장준표 님이 모두에게:    오후 1:36
+강사님 이전 강의자료도 swedu에 올려 주실수 있나요?
+강사 김대경 님이 모두에게:    오후 1:37
+네..pdf로 변환해 올려드리도록 하겠습니다.
+BS_장준표 님이 모두에게:    오후 1:37
+감사합니다.
+vs하창완 님이 모두에게:    오후 2:15
+윈도우에서 kuberentes 연결해서 docker안쓰고 python script만 업데이트하면서 쓰고 싶은데..가능할까요?
+vs하창완 님이 모두에게:    오후 2:18
+윈도우 툴에 대한 의존성이 있고, 하나의 노드에 하나의 프로그램만 써야 해서요.
+vs하창완 님이 모두에게:    오후 2:18
+테스트 장비가 여러대인데.. 하드웨어하고 묶여있어서 여러개가 실행이 안되요
+vs하창완 님이 모두에게:    오후 2:19
+서버하나에 하나의 제품 hw가 있습니다.
+vs하창완 님이 모두에게:    오후 2:20
+테스트 구성하고 hw setting이 유동적이라. python 수행 script를 쿠버네티스에서 script 버젼에따라 관리를 한번 해보고싶어서요. 
+vs하창완 님이 모두에게:    오후 2:22
+넵
+vs하창완 님이 모두에게:    오후 2:25
+아. 넵 
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 2:51
+강사님, 질문이 하나 있습니다. 예를들어 동일한 webserver를 replica를 이용해서 5개의 pod을 띄웠다고 했을 때, 외부에서 접속할때 실제 어디로 접속하게 되는지는 kube가 알아서 벨런싱 해주는건가요?
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 2:51
+그걸 가능하게 해주는게 loadbalancer인건인지요?
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 2:53
+nodeport를 사용할 경우에는 pod마다 외부 port를 무조건 다르게 지정해줘야 하는건지요?
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 2:54
+두개의 차이가 무엇인지 궁금합니다
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 2:58
+음.. 내부 아이피:포트를 외부로 포워딩 해주는 개념은 이해하였는데, 동일한 pod이 여러개 있을 때 실제로 어디로 접속되는지가 궁금했었습니다. 그건 kube가 알아서 해준다고 이해하면 되는것이지요?
+민경직/선임/webOS Platform Task 님이 모두에게:    오후 2:58
+감사합니다
+강사 김대경 님이 모두에게:    오후 3:20
+참 위 코드는 /templates/configmap.yaml 파일의 형식으로 사용하게 됩니다.
+강사 김대경 님이 모두에게:    오후 3:20
+관련 자료 url: https://helm.sh/ko/docs/chart_template_guide/getting_started/
+구세완 님이 모두에게:    오후 3:24
+감사합니다... 혹시 지금까지 적용해보신 것 중에서 직접 레퍼런스할 수 있는 것이 있으면 공유부탁드리며, 아니면 일단 인지만 하고 있겠습니다.
+강사 김대경 님이 모두에게:    오후 3:25
+안녕하세요?
+강사 김대경 님이 모두에게:    오후 3:26
+네 swedu에 올려드리도록 하겠습니다.
